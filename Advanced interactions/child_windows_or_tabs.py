d732fb4a6a316833dda0163d5e56cea0bbf383e8
madhumitha281 <132542962+madhumitha281@users.noreply.github.com>
2025-07-03 20:34:50 +0530

    Update child_windows_or_tabs.py

--- Advanced interactions/child_windows_or_tabs.py
+++ Advanced interactions/child_windows_or_tabs.py
@@ -11,12 +11,38 @@ from selenium.webdriver.common.action_chains import ActionChains
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://the-internet.herokuapp.com/windows")
+driver.maximize_window()
+WebDriverWait(driver,5).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT,"Click"))).click()
+
 
 # Get list of all open window handles (main + child windows/tabs)
-handles = driver.window_handles
+windowsOpened = driver.window_handles
 
 # Store the handle of the main/original window
 main_window = driver.current_window_handle
 
 # Switch to the second window/tab (usually the new one)
-driver.switch_to.window(handles[1])  # or whichever index you need
+#method1: note that your current window(mainwindow is in 0th index)
+driver.switch_to.window(windowsOpened[1])
+time.sleep(2)
+#method2: when you have many
+"""
+for i in handles:
+    if i!= main_window:
+        driver.switch_to.window(i)
+        break
+"""
+
+#print the text present in the child window:
+childwindowText = driver.find_element(By.XPATH,"//h3")
+print(childwindowText.text)
+
+#close the child window
+driver.close()
+
+#return to main window
+driver.switch_to.window(main_window)
+print(driver.find_element(By.XPATH,"//h3").text)
+
+
+time.sleep(2)
