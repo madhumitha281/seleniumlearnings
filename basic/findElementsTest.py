0030e8806d880605d489e52bc59ded892324566a
madhumitha281 <132542962+madhumitha281@users.noreply.github.com>
2025-07-27 00:57:50 +0530

    Delete basic/findElementsTest.py

--- basic/findElementsTest.py
+++
@@ -1,9 +0,0 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-driver.get("https://www.makemytrip.com/")
